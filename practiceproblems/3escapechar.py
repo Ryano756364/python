cb6e693a4f990041a5e34ad9373f9e81eeaf5bb9
rodell <ryanodell2668@gmail.com>
2022-12-30 15:48:23 -0500

    practice problems added

---
+++ practiceproblems/3escapechar.py
@@ -0,0 +1,21 @@
+splitString = "This string have been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4"
+print(tabbedString)
+
+# To use single quotes inside a string literally already created with single quote '
+print('The pet shop owner said "No, no, \'e\'s uh, ...he\'s resting".')
+print("""This also works too for for '' and "" inside a string literal""")
+
+anotherSplitString = """This string has been 
+split 
+over
+several line"""
+print(anotherSplitString)  # prints exactly how it's typed out here
+
+anotherSplitString = """This string has been \
+split \
+over \
+one line"""
+print(anotherSplitString)  # prints one line
