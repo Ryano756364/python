2047689093c7fe3232f81436b4be23ce97b4140e
rodell <ryanodell2668@gmail.com>
2022-12-30 15:37:03 -0500

    practice problems branch opened up

---
+++ practiceproblems/1helloworld.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print(1 + 2)
+print(7 * 6)
+print("The end", "or is it?", "keep watching to learn more about Python", 3)
+
+# print is a function
+# string literal is the 'hello, world argument' passed into the print function
