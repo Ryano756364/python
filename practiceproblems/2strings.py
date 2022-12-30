fece43876fb74539312cc454e13c997ba9cd13cc
rodell <ryanodell2668@gmail.com>
2022-12-30 15:48:07 -0500

    practice problems added

--- practiceproblems/2strings.py
+++ practiceproblems/2strings.py
@@ -2,3 +2,13 @@ print("Today is a good day to learn Python")
 print('Python is fun')
 print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
+
+# we can concatenate too
+print("hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+
+print(greeting + " " + name)
+
+
