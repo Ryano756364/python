d82538d5c6b81f5654646ed7647f12663d512cd2
rodell <ryanodell2668@gmail.com>
2023-01-01 14:28:51 -0500

    4.60 | more range examples | step | step backwards | starting shortcuts

--- practiceproblems/Section4/10ranges.py
+++ practiceproblems/Section4/10ranges.py
@@ -7,3 +7,26 @@ for i in range(1, 20):  # usually you want to avoid single case variables except
 
 for i in range(1, 21):  # here's how we include 20
     print("i is now {}".format(i))
+
+for i in range(21):  # default value is 0 if no start value is included
+    print("i is now {}".format(i))
+
+# step value
+for i in range(0, 10, 2):  # steps 2
+    print("i is now {}".format(i))
+# prints
+# i is now 0
+# i is now 2
+# i is now 4
+# i is now 6
+# i is now 8
+
+# count backwards
+for i in range(10, 0, -2):  # steps 2
+    print("i is now {}".format(i))
+# prints
+# i is now 10
+# i is now 8
+# i is now 6
+# i is now 4
+# i is now 2
