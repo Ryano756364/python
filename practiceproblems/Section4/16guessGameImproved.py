d8326ba138bfc6cac8f75b0be4dbf27c37ae837f
rodell <ryanodell2668@gmail.com>
2023-01-01 16:15:17 -0500

    4.69 | guessing game improved

--- practiceproblems/Section4/16guessGameImproved.py
+++ practiceproblems/Section4/16guessGameImproved.py
@@ -4,13 +4,22 @@ import random
 
 highest = 10
 answer = random.randint(1, highest)
+quitGame = False
 
-
-print("Guess number between 1 and {}: ".format(highest))
-guess = int(input())    # IntelliJ knows that this code is simliar to other file
+print("Guess number between 1 and {}: (type '0' to exit the program) ".format(highest))
 
 # use while loop to allow player to keep guessing
+while not quitGame:
+    guess = int(input())
 
-
+    if guess == 0:
+        break;
+    elif guess == answer:
+        print("You got it! ")
+        break
+    elif guess < answer:
+        print("Please guess higher (or q) ")
+    else:
+        print("Please guess lower (or q) ")
 
 # generate random number
