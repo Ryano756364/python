7c07644594d54554fc87caf8c7fd2364ca43753d
rodell <ryanodell2668@gmail.com>
2023-01-01 13:28:32 -0500

    4.52 | checking IN and NOT IN

---
+++ practiceproblems/Section4/6notIn.py
@@ -0,0 +1,7 @@
+activity = input("What would you like to do today? ")
+
+# casefold checks for case sensitivity
+if "cinema" not in activity.casefold():
+    print("Looks like we are not going to the cinema")
+
+# search string methods in the documentaiton under stdtypes
