cf13fa5936557ab67df3d88420ab6080a42c6ca8
rodell <ryanodell2668@gmail.com>
2023-01-01 17:09:35 -0500

    4.76 | else in a for loop

---
+++ practiceproblems/Section4/19contrived.py
@@ -0,0 +1,11 @@
+numbers = [1, 45, 33, 12, 60]
+
+for number in numbers:
+    if number % 8 == 0:
+        # reject the list
+        print("The numbers unacceptable")
+        break
+else:
+    print("All those numbers are fine")
+
+# else can be added to for loop
