5e90c6cf01e40d80a0d3a180176911de1ac8391a
rodell <ryanodell2668@gmail.com>
2023-01-01 12:31:49 -0500

    4.45 | short debugging video

--- practiceproblems/Section4/2guessingGame.py
+++ practiceproblems/Section4/2guessingGame.py
@@ -8,5 +8,7 @@ if guess < answer:
     print("Please guess higher")
 elif guess > answer:    # could've use a second if here
     print("Please guess lower")
-else:
+else:   # if number is 5, else does not get evaluated, it goes striaght into the suite
     print("You got it first time")
+
+# this video showed intro steps to debugging
