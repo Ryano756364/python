1ceca0e7919467776269ef892279f842a5743662
rodell <ryanodell2668@gmail.com>
2023-01-01 15:55:09 -0500

    4.67 | break | converting text to lowercase

--- practiceproblems/Section4/15adventure.py
+++ practiceproblems/Section4/15adventure.py
@@ -3,10 +3,15 @@ available_exits = ["north", "south", "east", "west"]
 chosen_exit = ""
 
 while chosen_exit not in available_exits:
-    chosen_exit = input("Please choose a direction: ")
+    chosen_exit = input("Please choose a direction: (or 'quit' to quit) ")
+    if chosen_exit.casefold() == "quit":    # converts chosen_exit to lowercase
+        print("Game over")
+        break
 
 print("Aren't you glad you got out of there? ")
 
 # while loops are good when you don't know how many loops
 # or for reading data from file or an internet stream
 # good for file input and output
+
+# break | converting text to lowercase
