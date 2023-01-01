3faf06748b15fc5de9a37600071a7cf73dfaf4b1
rodell <ryanodell2668@gmail.com>
2023-01-01 16:06:45 -0500

    4.68 | generate random number | whie loop challenge setup

---
+++ practiceproblems/Section4/16guessGameImproved.py
@@ -0,0 +1,16 @@
+# random comes from a module called 'random'
+# import allows us to use all the objects from that module
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+
+
+print("Guess number between 1 and {}: ".format(highest))
+guess = int(input())    # IntelliJ knows that this code is simliar to other file
+
+# use while loop to allow player to keep guessing
+
+
+
+# generate random number
