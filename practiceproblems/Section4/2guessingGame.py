6379d896fbbcf88962d7b2ba897d93aa4ff047ac
rodell <ryanodell2668@gmail.com>
2023-01-01 12:28:33 -0500

    4.44 | setup for debugger | if, elif, else

---
+++ practiceproblems/Section4/2guessingGame.py
@@ -0,0 +1,12 @@
+answer = 5
+
+print("Please guess number between 1 and 10: ")
+# converts all their input to an int
+guess = int(input())
+
+if guess < answer:
+    print("Please guess higher")
+elif guess > answer:    # could've use a second if here
+    print("Please guess lower")
+else:
+    print("You got it first time")
