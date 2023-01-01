d0c6f6ec9db6c20bb1c15461bb6f42f45ceb17b6
rodell <ryanodell2668@gmail.com>
2023-01-01 15:35:02 -0500

    4.64 | for loop cleanup | .index()

--- practiceproblems/Section4/13searching.py
+++ practiceproblems/Section4/13searching.py
@@ -1,11 +1,20 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
-item_to_find = "spam"
+item_to_find = "albatross"  # will end up "Item found at None"
 found_at = None   # used as a constant to show variable doesn't have a value (similar to NULL in java)
 
-for index in range(len(shopping_list)):     # len() shows how many values in sequence
-    if shopping_list[index] == item_to_find:
-        found_at = index
-        break
+# better way to write this
+# for index in range(len(shopping_list)):     # len() shows how many values in sequence
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
 
-print("Item found at position {}".format(found_at))
+# the better way to write this
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
+# this can still be more efficient
+
+if found_at is not None:
+    print("Item found at position {}".format(found_at))
+else:
+    print("{} not found".format(item_to_find))
