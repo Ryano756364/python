faef878c7069990b64f2ec9a065ffb6e4002519b
rodell <ryanodell2668@gmail.com>
2023-01-01 13:03:10 -0500

    4.50 | and, or

--- practiceproblems/Section4/3conditions.py
+++ practiceproblems/Section4/3conditions.py
@@ -1,5 +1,13 @@
-age = int(input("How old are you? "))
+age = int(input("How old are you?? "))
 
-if age >= 16 and age <= 65:     # complex expression, need both to be true
-    print("Have a good day at work")
+if 16 <= age <= 65:     # complex expression, need both to be true
+    print("Have a good day at work!")
+else:
+    print("Enjoy your free time!")
 
+print("-" * 80)     # simple divider
+
+if age < 16 or age > 65:    # if first condition is hit, Python doesn't move onto second one (age > 65)
+    print("Enjoy your free time!")
+else:
+    print("Have a good day at work!")
