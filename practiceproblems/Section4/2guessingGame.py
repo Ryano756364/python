ca6aa566758e4b02b7c2ba918c5bfe26138f7e93
rodell <ryanodell2668@gmail.com>
2023-01-01 12:39:54 -0500

    4.46 | nest if, else

--- practiceproblems/Section4/2guessingGame.py
+++ practiceproblems/Section4/2guessingGame.py
@@ -6,8 +6,18 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())    # notice indent is the same, single = is involved with binding
+    if guess == answer:     # == tests equality
+        print("well done, you guessed it")
+    else:
+        print("Sorry you have not guessed correctly")
 elif guess > answer:    # could've use a second if here
     print("Please guess lower")
+    guess = int(input())  # (duplicate code with this block?)
+    if guess == answer:  # == tests equality
+        print("well done, you guessed it")
+    else:
+        print("Sorry you have not guessed correctly")
 else:   # if number is 5, else does not get evaluated, it goes striaght into the suite
     print("You got it first time")
 
