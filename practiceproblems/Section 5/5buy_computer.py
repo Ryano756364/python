0de9c598f4cd07df25911a021f8ea8e07914602d
rodell <ryanodell2668@gmail.com>
2023-01-02 12:09:28 -0500

    5.96 | menu improved | printing out a menu

--- practiceproblems/Section 5/5buy_computer.py
+++ practiceproblems/Section 5/5buy_computer.py
@@ -5,24 +5,26 @@ available_parts = ["computer",
                    "mouse mat",
                    "hdmi cable",
                    "dvd drive"]
+
+# list comprehension - however this looks bad
+# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+
+
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))  # input will return a string
+
+print(valid_choices)
 current_choice = "-"
 computer_parts = []  # create an empty list, list to store items
 
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append("computer")
-        elif current_choice == '2':
-            computer_parts.append("monitor")
-        elif current_choice == '3':
-            computer_parts.append("keyboard")
-        elif current_choice == '4':
-            computer_parts.append("mouse")
-        elif current_choice == '5':
-            computer_parts.append("mouse mat")
-        elif current_choice == '6':
-            computer_parts.append("hdmi cable")
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
+
     else:
         print("Please add options from the list below:")
         # use this because enumerate returns pairs of values
