bce1987be66ea6ab2642feae80b5f4609d26de35
rodell <ryanodell2668@gmail.com>
2023-01-02 15:52:12 -0500

    5.116 | function signatures

---
+++ practiceproblems/Section 5/15function_signatures.py
@@ -0,0 +1,7 @@
+# function signature notes using print example
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+import sys
+
+x = 2
+print(x)  # 2
+print(x, sep=' ', end='\n', file=sys.stdout, flush=False)  # 2
