8e94429e8b4c3041fc536763fe0e21b69f86b65c
rodell <ryanodell2668@gmail.com>
2023-01-02 18:32:28 -0500

    5.127 | unpacking tuples by enumerating | 2nd example

--- practiceproblems/Section 5/6enumerate_example.py
+++ practiceproblems/Section 5/6enumerate_example.py
@@ -27,3 +27,20 @@ for t in enumerate("abcdefgh"):
 
 index, character = (0, "a")
 print(index, character)  # 0, a
+
+# example
+recipes = [ ("Banana Bread", "Bananas", "Bread", 2022),
+            ("Toast", "Butter", "Bread", 2023),
+            ("Cookies", "Dough", "Chocolate Chips", 2023),
+]
+
+# solution #1
+for recipe in recipes:
+    recipe_name, ingredient_one, ingredient_two, date_added = recipe
+    print("Recipe: {}, Ingredient #1: {}, Ingredient #2: {}, Date Added: {}"
+          .format(recipe_name, ingredient_one, ingredient_two, date_added))
+
+# or another solution more efficient, solution #2
+for recipe_name, ingredient_one, ingredient_two, date_added in recipes:
+    print("Recipe: {}, Ingredient #1: {}, Ingredient #2: {}, Date Added: {}"
+          .format(recipe_name, ingredient_one, ingredient_two, date_added))
