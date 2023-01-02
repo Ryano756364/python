949c6cae431f883a20c8ead72a26bc3ef46b336f
rodell <ryanodell2668@gmail.com>
2023-01-02 11:16:28 -0500

    5.89 | adding to list example | multiple references to same list

--- practiceproblems/Section 5/3mutable.py
+++ practiceproblems/Section 5/3mutable.py
@@ -11,3 +11,15 @@ print(id(shopping_list))  # 2198131660736
 
 # lists are mutable and Python was able to add an item without creating
 # a brand-new list
+
+# how many lists are on this screen?
+# answer: only 1 list
+# both names are bound to the same object
+
+a = b = c = d = e = f = another_list  # 8 references to the same list
+
+print(a)  # ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice', 'cookies']
+print("Adding cream")
+b.append(("cream"))
+print(c)  # ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice', 'cookies', 'cream']
+print(d)  # ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice', 'cookies', 'cream']
