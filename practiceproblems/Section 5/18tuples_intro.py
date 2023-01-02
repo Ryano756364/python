fc1d781d3080fe86dd7eb7c276f42dbb44cccab9
rodell <ryanodell2668@gmail.com>
2023-01-02 17:20:43 -0500

    5.122 | tuples intro | notes added

--- practiceproblems/Section 5/18tuples_intro.py
+++ practiceproblems/Section 5/18tuples_intro.py
@@ -13,3 +13,13 @@ print((name, year, "Python", 2022))  # (Ryan 2023 Python 2022)
 # such as the line immediately preceding this one
 
 # may be good practice to always use () with Tuples
+
+# Tuples are immutable unlike Lists
+# indexing of Tuples is the same as Lists
+# you still use [] to index them (watch this as it can be confusing)
+# Tuples use less memory than Lists
+# Tuples protect integrity of data
+
+# you can turn a Tupple into a List as below:
+list_form = list(t)
+print(list_form)  # ['a', 'b', 'c']
