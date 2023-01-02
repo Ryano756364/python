a09dd2bce9358b1a295f825af2cd249d9199b717
rodell <ryanodell2668@gmail.com>
2023-01-02 18:15:26 -0500

    5.124 | unpacking tuples by enumerating

--- practiceproblems/Section 5/6enumerate_example.py
+++ practiceproblems/Section 5/6enumerate_example.py
@@ -1,3 +1,29 @@
 # variable names don't matter
-for index, character in enumerate("abcdefgh"):
+# enumerate is very usefully when you need the index
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(t)
     print(index, character)
+
+# t prints 8 tuples
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
+# (3, 'd')
+# (4, 'e')
+# (5, 'f')
+# (6, 'g')
+# (7, 'h')
+
+# index, character prints
+# 0 a
+# 1 b
+# 2 c
+# 3 d
+# 4 e
+# 5 f
+# 6 g
+# 7 h
+
+index, character = (0, "a")
+print(index, character)  # 0, a
