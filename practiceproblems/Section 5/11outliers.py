20d75b0373a59d445184f64380ae478205aa7059
rodell <ryanodell2668@gmail.com>
2023-01-02 14:30:30 -0500

    5.105 | safely removing values from a list

--- practiceproblems/Section 5/11outliers.py
+++ practiceproblems/Section 5/11outliers.py
@@ -1,6 +1,6 @@
 data = [4, 5, 104, 105, 110, 120, 160, 260, 275]
 
-del data[0:2]
+# del data[0:2]
 print(data)  # [104, 105, 110, 120, 160]
 
 # or
@@ -9,11 +9,24 @@ min_valid = 100
 max_valid = 200
 
 # this will not work
-for index, value in enumerate(data):
-    if (value < min_valid) or (value > max_valid):
-        del data[index]
-
-print(data)  # [104, 105, 110, 120, 160, 275]
+# for index, value in enumerate(data):
+#     if (value < min_valid) or (value > max_valid):
+#         del data[index]
+#
+# print(data)  # [104, 105, 110, 120, 160, 275]
 
 # why is 275 there?
 # the loop already removed second to last position and 275 moved into 260's position
+
+
+# process the low values in the list, list must be ordered
+# no new list was created here
+stop = 0;
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break
+
+print(stop)  # for debugging
+del data[:stop]
+print(data)
