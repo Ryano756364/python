a1c85f1e99c44fdd73b8b7bb39ecfb5478c38aa3
rodell <ryanodell2668@gmail.com>
2023-01-02 11:27:08 -0500

    5.90 | min, max, count in list

---
+++ practiceproblems/Section 5/4number_lists.py
@@ -0,0 +1,14 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))  # 2
+print(max(even))  # 8
+print(min(odd))  # 1
+print(max(odd))  # 9
+
+print()
+print(len(even))  # 4
+print(len(odd))  # 5
+
+print()
+print("Mississippi".count("s"))
