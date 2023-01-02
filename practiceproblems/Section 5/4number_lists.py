2cb320390132754ff5a88ececed2b9b58af7b5c1
rodell <ryanodell2668@gmail.com>
2023-01-02 11:31:06 -0500

    5.91 | notes added on append and dot notation

--- practiceproblems/Section 5/4number_lists.py
+++ practiceproblems/Section 5/4number_lists.py
@@ -1,6 +1,7 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
+# min, max are functions
 print(min(even))  # 2
 print(max(even))  # 8
 print(min(odd))  # 1
@@ -12,3 +13,11 @@ print(len(odd))  # 5
 
 print()
 print("Mississippi".count("s"))
+
+# when we call a method, we tell it which object it's called on
+# s.append(x)
+# appending x to a list called s
+# we pass x as an argument, so that the method knows what to append
+# OOP is big with . notation
+# if method needs argument, you put it in the parenthesis
+
