43d7b1cd1ebe5f2f9e0fad1d6364d3be2178b7cf
rodell <ryanodell2668@gmail.com>
2023-01-02 14:45:10 -0500

    5.109 | removing items backwards

---
+++ practiceproblems/Section 5/12go_backwards.py
@@ -0,0 +1,13 @@
+# backwards can be a safer strategy with mutable objects
+
+data = [104, 101, 4, 105, 308, 5, 107, 100, 304, 203, 1, 304, 108]
+min_valid = 100
+max_valid = 200
+
+# middle -1 is to capture first item in list
+# third -1 is to operate backwards
+for index in range(len(data) -1, -1, -1):
+    if data[index] < min_valid or data[index] > max_valid:
+        del data[index]
+        print(index, data)
+print(data)
