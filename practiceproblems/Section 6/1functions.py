89dcc70e6398fc672077ab0628699eb26120fdae
rodell <ryanodell2668@gmail.com>
2023-01-03 13:12:42 -0500

    6.140 | parameters | formal parameters | loop passing arguments

--- practiceproblems/Section 6/1functions.py
+++ practiceproblems/Section 6/1functions.py
@@ -1,8 +1,15 @@
 # function starts with def, then name, then ()
-def multiply():
-    result = 10.5 * 4
+# x, y are parameters (or formal parameters)
+def multiply(x, y):
+    result = x * y
     return result
 
 
 # PEP 8 wants two blank lines at end of function
-print(multiply())
+# providing arguments is known as "passing arguments"
+print(multiply(10, 4))  # 40
+
+# loop example
+for val in range(1,5):
+    two_times = multiply(val, 2)
+    print(two_times)  # 2, 4, 6, 8
